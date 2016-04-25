d3f5f2ba968ec34385fbdeae931665e8ac9f345c
Dominic Plouffe <dominic.plouffe@gmail.com>
2016-04-25 17:24:25 -0400

    Removed useless parenthesis in ExtendedButton + added correct header

--- Taylor/UI/ExtendedButton.swift
+++ Taylor/UI/ExtendedButton.swift
@@ -1,10 +1,29 @@
+// Copyright (c) 2016, Mirego
+// All rights reserved.
 //
-//  TouchableButton.swift
-//  Taylor
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-//  Created by Hugo Lefrancois on 2015-08-24.
-//  Copyright (c) 2015 Mirego. All rights reserved.
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// - Neither the name of the Mirego nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
+//   specific prior written permission.
 //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 import UIKit
 
@@ -17,7 +36,7 @@ public class ExtendedButton: UIButton
         let yOffset = max(0, minimalTapableSize.width - frame.height)
         let xOffset = max(0, minimalTapableSize.height - frame.width)
 
-        if(yOffset > 0 || xOffset > 0) {
+        if yOffset > 0 || xOffset > 0 {
             return bounds.insetBy(dx: -xOffset / 2, dy: -yOffset / 2).contains(point)
         }
 
